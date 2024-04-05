900d2592276e420224782f59c1ba53d8a49fc05c
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2024-04-06 07:53:56 +0900

    feat: adding an order to the quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,5 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+
+  scope :ordered, -> { order(id: :desc) } # This scope will order the quotes by id in descending order
 end
--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = Quote.ordered.first # Reference to the first fixture quote
   end
 
   test "Showing a quote" do
