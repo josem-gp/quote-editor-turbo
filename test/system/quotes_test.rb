977b7eba47b5dff220f648390c7a01ea980b7d0d
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2024-04-06 07:57:21 +0900

    feat: adding cancel btn to form and updating tests so they pass

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -20,7 +20,7 @@ class QuotesTest < ApplicationSystemTestCase
     fill_in "Name", with: "Capybara quote"
 
     assert_selector "h1", text: "Quotes"
-    click_on "Create quote"
+    click_on "Submit"
 
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
@@ -34,7 +34,7 @@ class QuotesTest < ApplicationSystemTestCase
     fill_in "Name", with: "Updated quote"
 
     assert_selector "h1", text: "Quotes"
-    click_on "Update quote"
+    click_on "Submit"
 
     assert_selector "h1", text: "Quotes"
     assert_text "Updated quote"
