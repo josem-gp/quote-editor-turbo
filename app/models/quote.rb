6976b40f5bbea1406dd3dd3562e9266f8ac1e3c9
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2024-04-07 12:04:22 +0900

    feat: add bradcasting with background jobs

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,7 +4,9 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) } # This scope will order the quotes by id in descending order
 
   # For Broadcasting on create, update and destroy
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
