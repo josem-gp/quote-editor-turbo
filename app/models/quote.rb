d31dbd4fad77ac6606d5225435b54cc53d70d9dc
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2024-04-07 11:56:59 +0900

    feat: braodcasting when quote is created, updated or deleted

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,9 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) } # This scope will order the quotes by id in descending order
+
+  # For Broadcasting on create, update and destroy
+  after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
