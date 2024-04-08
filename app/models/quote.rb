a210381978cce9b7826fe94bd4af78753455a956
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2024-04-08 18:46:14 +0900

    feat: setup to learn about security with turbo

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ApplicationRecord
+  belongs_to :company
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) } # This scope will order the quotes by id in descending order
