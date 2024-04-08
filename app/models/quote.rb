0594205f48539a90dc8e6bc3c763ddc0c0f95738
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2024-04-08 19:14:16 +0900

    feat: specify the broadcast channel down to the company for security reasons

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -9,5 +9,6 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # Those three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  # We use [quote.company, "quotes"] for security reasons, to avoid broadcasting to the wrong company channel
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
